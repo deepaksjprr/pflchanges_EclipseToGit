a5c9d947d950784cadf868d65cf3f730557dc033
Deepak <deepak.sharma1@metacube.com>
2019-05-08 17:39:10 +0530

    pushing TS05 to main branch

--- pfl_framework/src/test/java/com/pfl/automation/org62/testcases/TS05_lightning.java
+++ pfl_framework/src/test/java/com/pfl/automation/org62/testcases/TS05_lightning.java
@@ -1,6 +1,5 @@
 package com.pfl.automation.org62.testcases;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.pfl.automation.base.BaseTest;
@@ -8,7 +7,8 @@ import com.pfl.automation.utils.Constants;
 
 public class TS05_lightning extends BaseTest{
 	
-	static By sflt_lightning_icon = By.cssSelector("a.switch-to-lightning");
+	//static By sflt_lightning_icon = By.cssSelector("a.switch-to-lightning");
+	//static By sflt_app_launcher = By.xpath ("//div[contains(@class, 'slds-icon-waffle')]/..");
 	static String program_string = "Random_auto_program";
 	static String admin_bd_auto_product_1 = "PFL_Auto_Product1";
 	static String admin_bd_auto_product_2 = "PFL_Auto_Product5";
@@ -25,11 +25,12 @@ public class TS05_lightning extends BaseTest{
 	static String program_name = program_string + time;
 	
 	@Test(priority=0, alwaysRun = true)
-	public void TS05_DataSet_Creation() throws Exception {
-
+	public void TS05_DataSet_Creation() throws Exception 
+	{
 		//create_setup_program
 		loginPage.logintoapplication(Constants.username1, Constants.password1);	
-		salsesforcePage.verify_lightning_page(sflt_lightning_icon);		
+		//salsesforcePage.verify_lightning_page(sflt_lightning_icon);	
+		salsesforcePage.sflt_verify_lightning_page(Constants.sflt_app_launcher, Constants.sflt_lightning_icon);	
 		salsesforcePage.open_tab_lightng(Constants.tab_swagiq_tools);
 		adminpage.sflt_click_program_admin_tile();
 		adminpage.sflt_set_new_program(driver, program_name);
@@ -42,71 +43,48 @@ public class TS05_lightning extends BaseTest{
 		homePage.logout_sflt();
 	}
 	
-		@Test(priority=1, alwaysRun = true)
-		public void TS05_01_verifying_send_swag_with_multiple_products() throws Exception {
-			loginPage.logintoapplication(Constants.sfnuer, Constants.sfnpass);
-			salsesforcePage.open_tab(Constants.tab_contacts);
-			salsesforcePage.selectview(Constants.pfl_contact_view);
-			salsesforcePage.opencontact(Constants.con_bd_auto_contact_first_name, Constants.con_bd_auto_contact_last_name);
-			salsesforcePage.click_sendswagbutton();
-			salsesforcePage.select_contact_address();
-			salsesforcePage.use_this_address();
-			salsesforcePage.verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_1);
-			salsesforcePage.click_program_on_send_swag_screen(program_name, admin_bd_auto_product_1);
-			swagiq.click_next_button_send_swag();
-			swagiq.click_send_marketing_confirmation_msg();
-			swagiq.click_send_marketing_materials_button();
-			swagiq.order_confirmation_msg();
-			
-			salsesforcePage.click_sendswagbutton();
-			salsesforcePage.select_contact_address();
-			salsesforcePage.use_this_address();
-			salsesforcePage.verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_2);
-			salsesforcePage.click_program_on_send_swag_screen(program_name, admin_bd_auto_product_2);
-			swagiq.click_next_button_send_swag();
-			swagiq.click_send_marketing_confirmation_msg();
-			swagiq.click_send_marketing_materials_button();
-			swagiq.order_confirmation_msg();
-			homePage.logout(driver);
-		
-		
-		
-		loginPage.logintoapplication(Constants.sfnuer, Constants.sfnpass);
-		salsesforcePage.open_tab(Constants.tab_contacts);
-		salsesforcePage.selectview(Constants.pfl_contact_view);
-		salsesforcePage.opencontact(Constants.con_bd_auto_contact_first_name, Constants.con_bd_auto_contact_last_name);
+	@Test(priority=1, alwaysRun = true)
+	public void TS05_verifying_send_swag_with_multiple_products() throws Exception 
+	{	
+		loginPage.logintoapplication(Constants.username1, Constants.password1);	
+		salsesforcePage.sflt_verify_lightning_page(Constants.sflt_app_launcher, Constants.sflt_lightning_icon);			
+		salsesforcePage.open_tab_lightng(Constants.tab_contacts);
+		salsesforcePage.sflt_select_view(Constants.pfl_contact_view);
+		salsesforcePage.sflt_open_contact(Constants.con_bd_auto_contact_first_name, Constants.con_bd_auto_contact_last_name);
 		salsesforcePage.click_sendswagbutton();
-		salsesforcePage.select_contact_address();
-		salsesforcePage.use_this_address();
-		salsesforcePage.verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_1);
-		salsesforcePage.click_program_on_send_swag_screen(program_name, admin_bd_auto_product_1);
-		swagiq.click_next_button_send_swag();
-		swagiq.click_send_marketing_confirmation_msg();
-		swagiq.click_send_marketing_materials_button();
-		swagiq.order_confirmation_msg();
+		salsesforcePage.sflt_select_contact_address();
+		salsesforcePage.sflt_use_this_address();
+		salsesforcePage.sflt_verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_1);
+		salsesforcePage.sflt_click_program_on_send_swag_screen(program_name, admin_bd_auto_product_1);
+		swagiq.sflt_click_next_button_send_swag();
+		swagiq.sflt_click_send_marketing_confirmation_msg();
+		swagiq.sflt_click_send_marketing_materials_button();
+		swagiq.sflt_order_confirmation_msg();
 		
 		salsesforcePage.click_sendswagbutton();
-		salsesforcePage.select_contact_address();
-		salsesforcePage.use_this_address();
-		salsesforcePage.verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_2);
-		salsesforcePage.click_program_on_send_swag_screen(program_name, admin_bd_auto_product_2);
-		swagiq.click_next_button_send_swag();
-		swagiq.click_send_marketing_confirmation_msg();
-		swagiq.click_send_marketing_materials_button();
-		swagiq.order_confirmation_msg();
-		homePage.logout(driver);
+		salsesforcePage.sflt_select_contact_address();
+		salsesforcePage.sflt_use_this_address();
+		salsesforcePage.sflt_verify_program_on_contact_send_marketing_page(program_name, admin_bd_auto_product_2);
+		salsesforcePage.sflt_click_program_on_send_swag_screen(program_name, admin_bd_auto_product_2);
+		swagiq.sflt_click_next_button_send_swag();
+		swagiq.sflt_click_send_marketing_confirmation_msg();
+		swagiq.sflt_click_send_marketing_materials_button();
+		swagiq.sflt_order_confirmation_msg();
+		homePage.logout_sflt();
 	}
 	
 	@Test(priority=2, alwaysRun = true)
-	public void TS05_DataSet_Reset() throws Exception {
+	public void TS05_DataSet_Reset() throws Exception 
+	{
 		//delete_order_send_swag
-		loginPage.logintoapplication(Constants.username, Constants.password);
-		salsesforcePage.open_tab(Constants.tab_contacts);
-		salsesforcePage.selectview(Constants.pfl_contact_view);
-		salsesforcePage.opencontact(Constants.con_bd_auto_contact_first_name, Constants.con_bd_auto_contact_last_name);
-		salsesforcePage.delete_order(admin_bd_auto_product_1, program_name, order_status);
-		salsesforcePage.delete_order(admin_bd_auto_product_2, program_name, order_status);
-		homePage.logout(driver);
+		loginPage.logintoapplication(Constants.username1, Constants.password1);	
+		salsesforcePage.sflt_verify_lightning_page(Constants.sflt_app_launcher, Constants.sflt_lightning_icon);		
+		salsesforcePage.open_tab_lightng(Constants.tab_contacts);
+		salsesforcePage.sflt_select_view(Constants.pfl_contact_view);
+		salsesforcePage.sflt_open_contact(Constants.con_bd_auto_contact_first_name, Constants.con_bd_auto_contact_last_name);
+		salsesforcePage.sflt_delete_order(admin_bd_auto_product_1, program_name, order_status);
+		salsesforcePage.sflt_delete_order(admin_bd_auto_product_2, program_name, order_status);
+		homePage.logout_sflt();
 	}
 
 }
